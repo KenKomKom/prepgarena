079832143ae7ba637e4becacdb07afbc0d92a1f5
KenKomKom <kenichikomala@gmail.com>
2025-01-20 18:44:15 +0700

    main menu button gacor

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,3 +1,35 @@
 extends Node
 
 var next_level: String
+#
+#func play_audio(file_path, pitch=1.0, volume = 0):
+	#if not audiostream2.playing :
+		#audiostream2.volume_db=volume
+		#audiostream2.pitch_scale = pitch
+		#audiostream2.stream = load(file_path)
+		#audiostream2.play()
+	#else :
+		#audiostream3.volume_db=volume
+		#audiostream3.pitch_scale = pitch
+		#audiostream3.stream = load(file_path)
+		#audiostream3.play()
+#
+#func play_audio_background(file_path, volume_db=0):
+	#if audiostream1.playing:
+		#var tween = get_tree().create_tween()
+		#tween.tween_property(audiostream1, "volume_db", -10, 1)
+		#await get_tree().create_timer(1).timeout
+		#audiostream1.stop()
+	#audiostream1.volume_db=-10
+	#audiostream1.stream= load(file_path)
+	#audiostream1.play()
+	#var tween = get_tree().create_tween()
+	#tween.tween_property(audiostream1, "volume_db", volume_db, 1)
+	#await get_tree().create_timer(1).timeout
+	#audiostream1.volume_db=volume_db
+#
+#func stop_audio_background():
+	#var tween = get_tree().create_tween()
+	#tween.tween_property(audiostream1, "volume_db", -10, 1)
+	#await get_tree().create_timer(1).timeout
+	#audiostream1.stop()
--- scripts/buttonanimation.gd
+++ scripts/buttonanimation.gd
@@ -1,12 +1,11 @@
 extends Node
 
-@export var from_center : bool = true
-@export var hover_scale : Vector2 =Vector2.ONE
+@export var y_hover : int =0
 @export var time := 0.5
 @export var transition_type: Tween.TransitionType
 
-var target : Control
-var default_scale : Vector2
+var target
+var default_y
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,19 +13,28 @@ func _ready():
 	call_deferred("set_up")
 	target.connect("mouse_entered", on_hover)
 	target.connect("mouse_exited", off_hover)
+	target.connect("button_down", button_down)
 
 func set_up():
-	if from_center:
-		target.pivot_offset = target.size/2
-	default_scale = target.scale
+	default_y = get_parent().custom_minimum_size.y
 
 func on_hover():
-	add_tween("scale", hover_scale,time)
+	#add_tween(get_parent().get_theme_stylebox("hover"), "grow_begin", 10,time)
+	#add_tween(get_parent().get_theme_stylebox("hover"), "grow_end", 10,time)
+	add_tween(get_parent().get_child(0), "custom_minimum_size:x", 200, time)
+	add_tween(get_parent(), "custom_minimum_size:y", default_y+20, time)
 
 func off_hover():
-	add_tween("scale", default_scale ,time)
+	#add_tween(get_parent().get_theme_stylebox("hover"), "grow_begin", 0,time)
+	#add_tween(get_parent().get_theme_stylebox("hover"), "grow_end",0,time)
+	add_tween(get_parent().get_child(0), "custom_minimum_size:x", 0, time)
+	add_tween(get_parent(), "custom_minimum_size:y", default_y, time)
 
-func add_tween(property:String, value, seconds:float):
+func button_down():
+	add_tween(get_parent().get_child(0), "color", Color(Color.CADET_BLUE), time)
+
+func add_tween(target, property:String, value, seconds:float):
 	if get_tree()!=null:
 		var tween= get_tree().create_tween()
+		tween.set_parallel(true)
 		tween.tween_property(target, property, value, seconds).set_trans(transition_type)
